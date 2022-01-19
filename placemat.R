1fdfc68764ac63556e5b81c0a8465c8fe714d65e
nosimpler <nosimpler@gmail.com>
2022-01-19 09:04:34 -0500

    initial commit

---
+++ placemat.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(broom)
+library(NMF)
+
+#TODO: breaks if any input columns start with W or H
+#TODO: output columns are unsorted
+#TODO: better variable names
+
+
+replw <- function(str) {str_replace(str, 'V', 'W')}
+replh <- function(str) {str_replace(str, 'V', 'H')}
+
+tidy_nmf <- function(df, n, val, h = c(), w = c()){
+  hrowname <- select(df, h)
+  wcolname <- select(df, w)
+  usecols <- c(h, w, val)
+  id_cols <- setdiff(names(df), usecols)
+  
+  mat <- df %>%
+    pivot_wider(names_from = h, values_from = val) %>%
+    select(where(is.numeric), -id_cols) %>%
+    as.matrix()
+  
+  nm  <- nmf(mat, n) 
+  
+  H <- nm %>%
+    pluck("fit") %>%
+    pluck("H") %>%
+    t() %>%
+    as_tibble() %>%
+    rename_with(replh) %>%
+    cbind(hrowname) %>%
+    pivot_longer(starts_with("H"), names_to = "H", values_to = "Hvalue")
+  W <- nm %>% 
+    pluck("fit") %>%
+    pluck("W") %>%
+    as_tibble() %>%
+    rename_with(replw) %>%
+    cbind(wcolname) %>%
+    pivot_longer(starts_with('W'), names_to = "W", values_to = "Wvalue")
+
+  cbind(W,H)
+}
+
+
+df <- tribble(~TIME, ~AXIS, ~FILE1, ~FILE2, ~FILE3,
+        1, 'x', 1, 2, 3,
+        1, 'y', 4, 5, 6,
+        1, 'z', 7, 8, 9,
+        2, 'x', 3, 2, 1,
+        2, 'y', 6, 5, 4,
+        2, 'z', 12, 8, 7
+        )
+
+df <- df %>%
+  pivot_longer(cols = starts_with('FILE'), names_to = "FILE", values_to = 'COUNTS')
+
+h <- c('TIME', 'AXIS')
+w <- 'FILE'
+tn <- tidy_nmf(df, 2, h = h, w = w, val = 'COUNTS')
+
+
+
+
+
+
+
